11727341f659df47bade0b4070dffa97b2ed7d55
Igor Mammedov <imammedo@redhat.com>
2013-01-10 14:39:45 +0100

    print all cpuid words the qemu cares about
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>

--- shared/deps/cpuid_test_kernel/test.c
+++ shared/deps/cpuid_test_kernel/test.c
@@ -12,7 +12,54 @@
 
 #include "main.h"
 
+
+typedef struct {
+    unsigned int eax;
+    unsigned int count;
+} level_t;
+
+static level_t levels[] = {
+    { 0, 0 },
+    { 1, 0 },
+    { 2, 0 },
+    { 4, 0 },
+    { 4, 1 },
+    { 4, 2 },
+    { 5, 0 },
+    { 6, 0 },
+    { 7, 0 },
+    { 9, 0 },
+    { 0xA, 0 },
+    { 0xD, 0 },
+    { 0x80000000, 0 },
+    { 0x80000001, 0 },
+    { 0x80000002, 0 },
+    { 0x80000003, 0 },
+    { 0x80000004, 0 },
+    { 0x80000005, 0 },
+    { 0x80000006, 0 },
+    { 0x80000008, 0 },
+    { 0x8000000A, 0 },
+    { 0xC0000000, 0 },
+    { 0xC0000001, 0 },
+    { 0xC0000002, 0 },
+    { 0xC0000003, 0 },
+    { 0xC0000004, 0 },
+};
+
+
 void test()
 {
-    printf("dummy test\n");
+    unsigned int eax, ebx, ecx, edx, i;
+
+    printf("CPU:\n");
+    for (i=0; i < sizeof(levels)/sizeof(*levels); i++) {
+        asm("cpuid"
+            : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx)
+            : "a" (levels[i].eax), "c" (levels[i].count));
+
+        printf("   0x%08x 0x%02x: eax=0x%08x ebx=0x%08x"
+               " ecx=0x%08x edx=0x%08x\n", levels[i].eax,
+               levels[i].count, eax, ebx, ecx, edx);
+   }
 }
